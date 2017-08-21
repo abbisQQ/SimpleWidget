86adc26e7be105c18978b905062327f6d8278bd5
Ch. Theodorou <theodoroucharalabos@gmail.com>
2017-08-21 13:18:15 +0300

    Initial commit

---
+++ app/src/main/java/com/abbisqq/simplewidget/SimpleWidget.java
@@ -0,0 +1,50 @@
+package com.abbisqq.simplewidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SimpleWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
+        // Construct an Intent object includes web adresss.
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://codepen.io/Abbis/full/NRVbjX/"));
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
